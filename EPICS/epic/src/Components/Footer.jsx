75993b1680bd38ff00d37cb18f4c2ef0d42fc75d
FalguniJain <115800323+FalguniJain@users.noreply.github.com>
2024-12-07 18:11:08 +0530

    Add files via upload

---
+++ EPICS/epic/src/Components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <p>© 2024 Govt Scheme Finder | All Rights Reserved</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ EPICS/epic/src/Components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <h1>Govt Scheme Finder</h1>
+      <p>Find the perfect scheme tailored for you!</p>
+    </header>
+  );
+}
+
+export default Header;
